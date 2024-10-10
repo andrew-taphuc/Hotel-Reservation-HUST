c03c30e483ae278229142b647be0a384a9563b56
Ta Hong Phuc <taphuc1@gmail.com>
2024-10-10 16:10:44 +0700

    psql vercel

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,15 @@
 const { Pool } = require("pg");
  
-const pool = new Pool()
- 
+// const pool = new Pool()
+const { Pool } = require('pg')
+require('dotenv').config()
+
+const pool = new Pool({
+  connectionString: "postgres://default:53LYgWVROBbl@ep-yellow-morning-a4skmabh-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
+})
+
+
+
 module.exports ={
      query: (text, params) => pool.query(text, params) 
 };
